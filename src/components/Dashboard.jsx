e803c2399f6f8acd140b162ed4c3f16a7f81107e
RChakradharii <renukachakradhari23@navgurukul.org>
2024-10-16 16:19:28 +0530

    Updated CSS and some JS for responsiveness

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Dashboard.css';
 
 const Dashboard = () => {
+    const [selectedLocation, setSelectedLocation] = useState("Dantewada");
+
+    const handleLocationChange = (event) => {
+        setSelectedLocation(event.target.value);
+    };
+
     return (
         <div className="dashboard-container">
-            <h2 className='dashboard-heading'>Dashboard Overview</h2>
+            <div className="dashboard-header">
+                <h2 className='dashboard-heading'>Dashboard Overview</h2>
+                <select className="location-dropdown" value={selectedLocation} onChange={handleLocationChange}>
+                    <option value="Dantewada">Dantewada</option>
+                    <option value="Geedam">Geedam</option>
+                    <option value="Barsoor">Barsoor</option>
+                </select>
+            </div>
             <div className="dashboard-stats">
                 <div className="dashboard-card">
                     <h3>Total Bookings</h3>
@@ -15,7 +28,7 @@ const Dashboard = () => {
                     <p>00</p>
                 </div>
                 <div className="dashboard-card">
-                    <h3>Expenses</h3>
+                    <h3>Total Rent</h3>
                     <p>/-</p>
                 </div>
             </div>
